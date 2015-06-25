21c7cf31a17ecfea67ba6ee0b6b0e77ff1c0f459
Sebastiaan Bakker <bakker.sebastiaan@gmail.com>
2015-06-25 20:01:16 +0200

    Improving code for ajax functions

--- static/blackbook.js
+++ static/blackbook.js
@@ -1,24 +1,8 @@
+// Blackbook Javascript
+
+
      // A $( document ).ready() block.
 $( document ).ready(function() {
-
-// EXAMPLE CODE....NOT IN USE
-// Add a phone number input field
-var $counter = 0;
-$( "#add_phone" ).click(function( event ) {
-    event.preventDefault();
-    $counter = $counter +1;
-    console.log($counter)
-    if($counter < 4) {
-        var section = $("#phonetemplate").clone();
-        section.attr('id', 'phone' + $counter);
-        $( ".addstuff" ).append( $( section ) );
-        // still need to change params in the cloned input field
-    }
-    else {
-        $("#warning-field").html("You can only add 3 phone numbers here.");
-        }
-});
-// end of adding phone number
 
 
 
@@ -119,65 +103,64 @@ $(document).on('click', ".list-group-item", function () {
 });
 
 // Buttons on the "New Page" to add a new person or a business.
+
+// On the "New page" the user clicked a button to add a new person or 
+// business. Now we do an Ajax request to process this button click.
+// A new page should be loaded giving the user a result.
 $(document).on('click', ".btn", function (event) {
     event.preventDefault();
     var $xxx = $( this ).data( "target" );
     if( $xxx == "new_person") {
-        console.log("We want a new person");
-        var $newdata = $( "#new_person" ).serialize().replace(/\+/g,'%20');
-        new_ajax($xxx, $newdata);
+        console.log("We want a new person");      
+        var $newdata = $( "#new_person" ).serializeArray();
+        ajax_load_page("/NewPerson", $newdata);
     }
     else if($xxx == 'new_business') {
         console.log("We want a new business");
+        var $newdata = $( "#new_business" ).serializeArray();
+        ajax_load_page("/NewBusiness", $newdata);
     }
     else {
         console.log("We want something else");
-    }
-    
-    
+    }  
 });
 
+// The Ajax function to load a "Page". We supply an url and some form data 
+// and the server can do the rest.
+function ajax_load_page($url, $data) {
+    // Using the core $.ajax() method
+    $.ajax({  
+        url: $url, 
+        data: $data,     // will be converted to a query string
+        type: "POST", 
+        dataType : "html", // The type of data we expect back
 
-// ajax
-function new_ajax($target, $newdata) {
-// Using the core $.ajax() method
-$.ajax({
-    // The URL for the request
-    url: "/newajax",
-    // The data to send (will be converted to a query string)
-    data: {
-        action: $target,
-        newdata: $newdata
-    },
+        // Code to run if the request succeeds.
+        success: function( result ) {
+            $("#full_page").html(result);
+        },
 
-    // Whether this is a POST or GET request
-    type: "POST",
-    // The type of data we expect back
-    dataType : "html",
-
-
-    // Code to run if the request succeeds;
-    // the response is passed to the function
-    success: function( json ) {
-        $("#full_page").html(json).slideDown();
-        //$( "<h1>" ).text( json.title ).appendTo( "body" );
-        //$( "<div class=\"content\">").html( json.html ).appendTo( "body" );
-    },
-
-    // Code to run if the request fails; the raw request and
-    // status codes are passed to the function
-    error: function( xhr, status, errorThrown ) {
-        alert( "Sorry, there was a problem!" );
-        console.log( "Error: " + errorThrown );
-        console.log( "Status: " + status );
-        console.dir( xhr );
-    },
-
-    // Code to run regardless of success or failure
-    complete: function( xhr, status ) {
-    //alert( "The request is complete!" );
-    }
-});
+        // Code to run if the request fails; the raw request and
+        // status codes are passed to the function
+        error: function( xhr, status, errorThrown ) {
+            alert( "Sorry, there was a problem!" );
+            console.log( "Error: " + errorThrown );
+            console.log( "Status: " + status );
+            console.dir( xhr );
+        },
+        // Code to run regardless of success or failure
+        complete: function( xhr, status ) {
+        //alert( "The request is complete!" );
+        }
+    });
 }
 
 
+// Some random comments and code samples.
+
+//Seems difficult to send form data :-O
+//$newdata.push({name: "action", value: "new_person"});
+//$newdata = jQuery.param($newdata);
+//.replace(/\+/g,'%20');
+
+// End of file
